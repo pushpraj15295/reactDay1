249ad61c8abae2fc44c76c031e3457dba8725955
pushpraj15295 <pushpraj15295@gmail.com>
2022-05-16 23:10:14 +0530

    add

---
+++ mobile/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <ul>Mobile Opreting System
+    <li>Android</li>
+    <li>BlackBerry</li>
+    <li>iPhone</li>
+    <li>windows phone</li>
+
+</ul>
+
+<ul>Mobile Menufatures
+    <li>Samsung</li>
+    <li>HTC</li>
+    <li>micromax</li>
+    <li>Apple</li>
+
+</ul>
+</>
+  );
+}
+
+export default App;
